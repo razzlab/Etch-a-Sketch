aea3bbbc30d5ca948f9e7e0fc9086786a07b6cb1
Raj <tiwariraj91929@gmail.com>
2024-01-03 18:26:18 +0530

    added query selector for colors and random,singe

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-const mainDiv = document.querySelector('.main')
+const grid = document.querySelector('.grid')
+const userColorPick = document.querySelector('#ink').value
+const randomDiv = document.querySelector('.random')
+randomDiv.textContent = "single"
+const randomOrPick = document.querySelector('.random').innerHTML
+randomDiv.textContent = "single"
+
+
+
+
+
+console.log(grid,userColorPick,randomOrPick)
+
+
 
