10b15e2cf9ca1c1bf3cf9f1c7eae089e8c6e9e0a
Raj <tiwariraj91929@gmail.com>
2024-01-03 18:59:01 +0530

    completed reset button

--- script.js
+++ script.js
@@ -4,14 +4,29 @@ const randomDiv = document.querySelector('.random')
 randomDiv.textContent = "single"
 const randomOrPick = document.querySelector('.random').innerHTML
 randomDiv.textContent = "single"
+const resetButton = document.querySelector('.reset')
+const setGrid = document.querySelector('.grid-size')
+let xgridx = 16;
 
 const color = () => {
-    if(randomOrPick=="singl"){
+    if(randomOrPick=="single"){
         return userColorPick
     }else{
         return ("#" + Math.floor(100000 + Math.random() * 900000))
     }
 }
+
+setGrid.addEventListener('click', ()=>{
+    xgridx = prompt("Please enter grid size", xgridx);
+})
+
+
+resetButton.addEventListener('click',()=>{
+    xgridx = 16;
+    randomDiv.textContent = "single";
+    userColorPick.textContent = "#000000"
+});
+
 
 
 
